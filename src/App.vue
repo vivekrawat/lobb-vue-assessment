a080f06e13d7f745c19b30e20bf171df0c97b144
vivek <vivek.rawat@anrxsolutions.com>
2025-06-20 17:52:21 +0530

    feat/some-basic-setup-with-tailwindcss-zod-vee-validate

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-background h-screen w-screen flex flex-col text-white">
+      <!-- <div class="border-b-2 border-primary h-16">
+        <h1 class="text-2xl">Lulla</h1>
+      </div> -->
+      <!-- <nav>
+        <RouterLink to="/"><p class="bg-red-500"> lulla</p>Home</RouterLink>
+        <RouterLink to="/about"><p class="bg-primary text-background"> lulla</p>About</RouterLink>
+      </nav> -->
+      <!-- <RouterView /> -->
+      <router-view v-slot="{ Component }"></router-view>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+import { useForm } from 'vee-validate';
+import { z } from 'zod';
+import { toTypedSchema } from '@vee-validate/zod';
+
+const LoginSchema = z.object({
+  email: z.string().min(1, 'Email is required').email('Invalid email address')
+})
+
+const errors = ref<Record<string,string>>({})
+const form = useForm({
+  validationSchema: toTypedSchema(
+    LoginSchema
+  ),
+  validateOnMount: false,
+  initialValues: {
+    email: ''
+  }
+});
+
+const onSubmit = () => {
+  form.handleSubmit(values => {
+    console.log(values)})
+}
+
+const canSubmit = computed(() => {
+  try {
+    LoginSchema.parse(form.values)
+    errors.value = {}
+    return true
+  } catch(err:any) {
+    type Error = {path: string[], message: string}
+    err.errors.forEach((error: Error) => {
+      errors.value[error.path[0]] = error.message
+    })
+    return false
+  }
+})
+
+const handleChange = (key: 'email', val: EventTarget | null) => {
+  const value =
+    val && 'value' in val && typeof val.value === 'string' ? val.value : '';
+  form.setFieldValue(key, value);
+};
+
+</script>
+
+<template>
+  <div class="h-screen flex items-center justify-center p-2">
+    <div class="flex flex-col justify-center w-full max-w-[500px] bg-input rounded-lg p-3 sm:p-10">
+      <div class="flex items-center justify-center">
+        <img src="@/assets/zoro.png" class="h-[150px] object-contain" alt="">
+        <!-- <div class="text-3xl text-primary font-extrabold">XORO</div> -->
+      </div>
+      <div class="mt-5">
+        <form @submit="onSubmit">
+          <input type="text" @input="(e) => handleChange('email', e.target)" placeholder="Email" class="bg-background text-white w-full text-md p-3 border-2 border-background rounded-lg">
+          <p v-show="!canSubmit && form.values['email']" class="text-destructive capitalize text-[14px] mt-1 ml-1">{{errors['email']}}</p>
+          <button :disabled="!canSubmit" class="cursor-pointer ext-lg bg-primary disabled:opacity-60 mt-10 w-full uppercase font-extrabold py-4 rounded-lg text-background ring-none">continue</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
