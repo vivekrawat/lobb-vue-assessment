30b3900eaf01b4aa90c0dfaf2c147bc026c95f90
vivek <vivek.rawat@anrxsolutions.com>
2025-06-22 05:33:54 +0530

    feat/added-some-route-validatons

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,13 +12,27 @@ const router = createRouter({
       path: '/',
       name: 'Content',
       component: () => import('../views/Content.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/details',
       name: 'Details',
       component: () => import('../views/ContentDetails.vue'),
+      meta: { requiresAuth: true }
     },
   ],
 })
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = sessionStorage.getItem('token');
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'Login' });
+  } else if(to.path === '/login' && isAuthenticated){
+    next({ name: 'Content' });
+  } else {
+    next();
+  }
+});
 
 export default router
